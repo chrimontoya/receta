abbc80e7ce1a82a006fa6358f36bfc98ec64711c
Christian montoya <christian.montoya@systemproject.cl>
2023-12-04 01:04:41 -0300

    feat: proyecto base con scss

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from '../app/modules/login/pages/login/login.component'
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent }
+];
