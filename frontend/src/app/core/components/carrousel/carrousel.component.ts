5d96c619d303e5ebb5de1483f39e31e8e4089f25
Christian montoya <christian.montoya@systemproject.cl>
2023-12-04 21:44:23 -0300

    feat: se agrega angular material y carrousel componente base

---
+++ frontend/src/app/core/components/carrousel/carrousel.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {NgStyle} from "@angular/common";
+@Component({
+  selector: 'app-carrousel',
+  standalone: true,
+  imports: [MatButtonModule, NgStyle],
+  templateUrl: './carrousel.component.html',
+  styleUrl: './carrousel.component.scss'
+})
+export class CarrouselComponent {
+  //image content to select
+  firstBackground: string = 'red';
+  secondBackground: string = 'green';
+  thirdBackground: string = 'blue';
+  //current image
+  backgroundSelected: string = this.firstBackground;
+  //default colors buttons
+  firstButton: string = '#FF8E3C';
+  secondButton: string = '#FFFFFE';
+  thirdButton: string = '#FFFFFE';
+
+
+  changeSelection(position: number){
+    switch (position){
+      case 1:
+        this.firstButton = "#FF8E3C";
+        this.secondButton = "#FFFFFE";
+        this.thirdButton = "#FFFFFE";
+        this.backgroundSelected = this.firstBackground;
+        break;
+      case 2:
+        this.firstButton = "#FFFFFE";
+        this.secondButton = "#FF8E3C";
+        this.thirdButton = "#FFFFFE";
+        this.backgroundSelected = this.secondBackground;
+        break;
+      case 3:
+        this.firstButton = "#FFFFFE";
+        this.secondButton = "#FFFFFE";
+        this.thirdButton = "#FF8E3C";
+        this.backgroundSelected = this.thirdBackground;
+        break;
+    }
+  }
+}
--- frontend/src/app/modules/login/pages/login/login.component.ts
+++ frontend/src/app/modules/login/pages/login/login.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {CarrouselComponent} from "../../../../core/components/carrousel/carrousel.component";
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, CarrouselComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
