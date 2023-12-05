281b4d0c7d4a36ec392fd1c32e8a960016714260
Christian montoya <christian.montoya@systemproject.cl>
2023-12-05 00:31:54 -0300

    feat: componente login base

--- frontend/src/app/modules/login/pages/login/login.component.ts
+++ frontend/src/app/modules/login/pages/login/login.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-import {FormsModule} from "@angular/forms";
+import {Component} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {CarrouselComponent} from "../../../../core/components/carrousel/carrousel.component";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, CarrouselComponent],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CarrouselComponent,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  userControl: FormControl = new FormControl();
+  passControl: FormControl = new FormControl();
+
+  logIn(){
+
+  }
 
 }
