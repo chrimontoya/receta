47d3fe558395eacb897a85ca88adf2d03415f440
Christian montoya <christian.montoya@systemproject.cl>
2023-12-09 01:17:42 -0300

    feat: se agrega firebase

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,19 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend';
+  firebaseConfig = {
+    apiKey: "AIzaSyBicwWyy50fbOuGR8e39ZXrBS384KvM-uI",
+    authDomain: "receta-cbdf0.firebaseapp.com",
+    projectId: "receta-cbdf0",
+    storageBucket: "receta-cbdf0.appspot.com",
+    messagingSenderId: "253474104707",
+    appId: "1:253474104707:web:f5116ff59fa006bac102b6",
+    measurementId: "G-QE60HT1JM8"
+  };
+
+  constructor() {
+    const app = initializeApp(this.firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
+
 }
