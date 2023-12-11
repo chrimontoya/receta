0e25a8e6ce3ddfd2725881f8d7c620f7944b1059
Christian montoya <christian.montoya@systemproject.cl>
2023-12-10 23:38:48 -0300

    feat: se agrega nav y drawer

--- frontend/src/app/modules/home/pages/landing/landing.component.ts
+++ frontend/src/app/modules/home/pages/landing/landing.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.sass'
