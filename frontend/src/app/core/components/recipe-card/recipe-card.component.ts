9959068323d1e6f1ba19eaed41933e7781eaf24a
Christian montoya <christian.montoya@systemproject.cl>
2023-12-17 17:21:01 -0300

    style: correccion de estilos para card y background, colores

--- frontend/src/app/core/components/recipe-card/recipe-card.component.ts
+++ frontend/src/app/core/components/recipe-card/recipe-card.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import {MatGridListModule} from "@angular/material/grid-list";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-recipe-card',
@@ -11,11 +11,19 @@ import {NgIf} from "@angular/common";
     MatCardModule,
     MatButtonModule,
     MatGridListModule,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss'
 })
 export class RecipeCardComponent {
-  @Input() recipeCard: any;
+  @Input() recipeCard: any = {
+    title: "Hola mundo",
+    userName: "christian",
+    imageUrl: "https://img.freepik.com/foto-gratis/fresas-recien-recolectadas-campo-imagen-generada-ia_511042-1699.jpg?w=1480&t=st=1702431530~exp=1702432130~hmac=1bb143f7625de019e447a96c08dcab9c451ac3e4fa9c978718ed9518fb42d2c0",
+    description: "Esta es una descripcion de prueba para ver el contenido de un card para ver como queda y ver los espacios al rededor como tambien se comporta el texto dentro de este",
+    preparationTime: "24min"
+  };
+  public datos: any = [1,2,3];
 }
--- frontend/src/app/modules/home/pages/recipe/recipe.component.ts
+++ frontend/src/app/modules/home/pages/recipe/recipe.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {SideNavService} from "../../../../core/services/side-nav.service";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {RecipeCardComponent} from "../../../../core/components/recipe-card/recipe-card.component";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {ViewportRuler} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-recipe',
@@ -10,17 +13,26 @@ import {RecipeCardComponent} from "../../../../core/components/recipe-card/recip
   imports: [
     MatButtonModule,
     MatIconModule,
-    RecipeCardComponent
+    RecipeCardComponent,
+    MatGridListModule,
+    MatCardModule
   ],
   templateUrl: './recipe.component.html',
   styleUrl: './recipe.component.scss'
 })
 export class RecipeComponent {
+  cols: number = 1;
   constructor(
-    private sideNavService: SideNavService
+    private sideNavService: SideNavService,
+    private viewportRuler: ViewportRuler
   ) {
     this.sideNavService.changeSelection(0);
+    this.cols = Math.max(1, Math.min(Math.floor(this.viewportRuler.getViewportSize().width/325), 4));
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.cols = Math.max(1, Math.min(Math.floor(this.viewportRuler.getViewportSize().width/325), 4));
+  }
 
 }
